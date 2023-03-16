724ab080e23740d37a4cf1be37a9ae6118c20a12
timoshkkkk <hahatimhaha@gmail.com>
2023-03-16 21:42:01 +0600

    first commit

---
+++ bin/cycle_practical_work.dart
@@ -0,0 +1,67 @@
+    import 'dart:io';
+import 'dart:math';
+
+void main() {
+//   int num;
+//   for(num = 1; num <= 5; num ++) {
+//     print(num);
+//   }
+//   int num1;
+// for(num1 = 5; num1 >= 1 ; num1 --) {
+//   print(num1);
+// } 
+
+// for(int s = 1; s <= 10; s ++) {
+//   print('3 * $s = ${3 * s}');
+// }
+
+
+// int summ = 0;
+//   for(int i = 1; i <= 200; i++) {
+//     if (i % 17 == 0) {
+//       summ += i;
+//     }
+//   }
+// print(summ);
+
+
+//   for(int g = 10; g <= 20; g++) {
+//     print(g * g);
+//   }
+
+// for(int h = 20; h <= 50; h++) {
+//   if ( h % 3 == 0 && h % 5 != 0) {
+//     print(h);
+//   }
+// }
+//  int summ1 = 0;
+//   for(int f = 1; f <= 50; f++) {
+//     if (f % 5 == 0|| f % 7 == 0) {
+//       summ1 += f;
+      
+//     }
+//   }
+//   print(summ1);
+
+//   for (int a = 10; a <= 99; a++) {
+//     if(a % 4 == 0 && a % 6 != 0) {
+//       print(a);
+//     }
+    
+//   }
+//   int summ2 = 0;
+// for(int a = 100; a <= 200; a++) {
+//  if(a % 17 == 0) {
+//   summ2 += a;
+//  }
+// }
+// print(summ2);
+
+
+int summ3 = 0;
+for(int x = 1; x <= 4; x++ ) {
+  summ3 += x * x;
+  print(x);
+}
+print(summ3);
+}
---
+++ test/cycle_practical_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:cycle_practical_work/cycle_practical_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
